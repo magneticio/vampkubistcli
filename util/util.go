c32ba10063efa1e3cda97693120339c6e4a83595
berkgokden <berkgokden@gmail.com>
2018-12-11 12:46:27 +0100

    Added a utility method to download

--- util/util.go
+++ util/util.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
 	"github.com/ghodss/yaml"
@@ -81,4 +83,31 @@ func Convert(inputFormat string, outputFormat string, input string) (string, err
 		outputSourceString = string(prettyJSON.Bytes())
 	}
 	return outputSourceString, nil
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
