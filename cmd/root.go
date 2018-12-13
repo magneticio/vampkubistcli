4737e10324eb48aa1792bfc0189a27811175fb63
berkgokden <berkgokden@gmail.com>
2018-12-13 12:48:45 +0100

    increment version to 0.0.3

--- cmd/root.go
+++ cmd/root.go
@@ -51,7 +51,7 @@ var OutputType string
 var Debug bool
 var Hosts []string
 
-const Version string = "0.0.2"
+const Version string = "0.0.3"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
