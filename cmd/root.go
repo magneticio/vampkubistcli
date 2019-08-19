8f34db266a68cbce779777d2ee758e064ec2e547
berkgokden <berkgokden@gmail.com>
2019-08-19 11:15:24 +0200

    Increment version

--- cmd/root.go
+++ cmd/root.go
@@ -68,7 +68,7 @@ var kubeConfigPath string
 var TokenStore client.TokenStore
 
 // version should be in format d.d.d where d is a decimal number
-const Version string = "v0.0.48"
+const Version string = "v0.0.49"
 
 var AppName string = InitAppName()
 
